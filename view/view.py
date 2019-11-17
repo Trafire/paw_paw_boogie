cb4882d35b9d898ff61f58d51db2d790070877b7
Trafire <antoinewood@gmail.com>
2019-11-17 00:35:15 -0500

    views added 7

--- view/view.py
+++ view/view.py
@@ -1,21 +1,24 @@
 from model.model import map_to_grid
 import pygame
 
-def display(control_map):
-    gameDisplay = pygame.display.set_mode((800,600))
 
+def display(control_map):
+    gameDisplay = pygame.display.set_mode((800, 600))
     pygame.display.set_caption('Paw Paw Boogie')
     black = (0, 0, 0)
     blue = (0, 0, 255)
     red = (255, 0, 0)
     while True:
-
         for key in iter(control_map.keys()):
 
-            if type(control_map[key]) == tuple:
-                x,y = map_to_grid(key)
-                pygame.draw.rect(gameDisplay, blue, (x * 50, y * 50,  50 , 50 ))
+            if type(control_map[key]):
+
+                data = map_to_grid(key)
+                if type(data) == tuple:
+                    x, y = data
+                    if control_map[key]:
+                        pygame.draw.rect(gameDisplay, red, (x * 50, y * 50, 50, 50))
+                    else:
+                        pygame.draw.rect(gameDisplay, black, (x * 50, y * 50, 50, 50))
 
         pygame.display.update()
-
-
