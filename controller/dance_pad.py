213886a7bafc3329fa0c9aedf5ea40aabddf7f7c
Trafire <antoinewood@gmail.com>
2019-11-16 23:21:56 -0500

    init for main function

--- controller/dance_pad.py
+++ controller/dance_pad.py
@@ -5,20 +5,21 @@ def get_input():
     events = inputs.get_gamepad()
     for event in events:
         if event.ev_type == 'Key':
-            print(event.code, 1==event.state)
-            return (event.code, 1==event.state)
+            print(event.code, 1 == event.state)
+            return (event.code, 1 == event.state)
 
 
 def update_control_map(control_map):
-    i = get_input()
-    if i:
-        control_map[i[0]] = i[1]
-        return True
-    return False
+    def update():
+        i = get_input()
+        if i:
+            control_map[i[0]] = i[1]
+            return True
+        return False
+    while True:
+        update()
 
-control_map = {'BTN_BASE3': False, 'BTN_TRIGGER': True, 'BTN_BASE': False, 'BTN_THUMB2': False, 'BTN_BASE2': False, 'BTN_TOP': False, 'BTN_TOP2': False, 'BTN_BASE4': False, 'BTN_THUMB': False, 'BTN_PINKIE': False}
 
+key_map = {'BTN_BASE3': False, 'BTN_TRIGGER': False, 'BTN_BASE': False, 'BTN_THUMB2': False, 'BTN_BASE2': False,
+               'BTN_TOP': False, 'BTN_TOP2': False, 'BTN_BASE4': False, 'BTN_THUMB': False, 'BTN_PINKIE': False}
 
-while True:
-    if update_control_map(control_map):
-        print(control_map)
--- paw_paw_boogie.py
+++ paw_paw_boogie.py
@@ -1,1 +1,21 @@
 from controller import dance_pad
+from multiprocessing import Process, Manager
+import time
+
+def f(d):
+    print(d)
+    time.sleep(1)
+
+if __name__ == '__main__':
+    manager = Manager()
+    d = manager.dict(dance_pad.key_map)
+
+    p1 = Process(target=dance_pad.update_control_map, args=(d,))
+    p2 = Process(target=f, args=(d,))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+
+
+
