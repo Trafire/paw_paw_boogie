e8488d1ca1c183a990d1aa7d470785f5b8002cee
Trafire <antoinewood@gmail.com>
2019-11-17 00:12:28 -0500

    views added 1

--- controller/dance_pad.py
+++ controller/dance_pad.py
@@ -5,7 +5,6 @@ def get_input():
     events = inputs.get_gamepad()
     for event in events:
         if event.ev_type == 'Key':
-            print(event.code, 1 == event.state)
             return (event.code, 1 == event.state)
 
 
